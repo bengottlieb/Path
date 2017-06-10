16ee18ac5cb79bdfa61780cc64ac87454ffd70bb
Ben Gottlieb <ben@standalone.com>
2017-06-10 11:39:30 -0500

    Initial Commit

---
+++ Nevertheless/Nevertheless.h
@@ -0,0 +1,19 @@
+//
+//  Nevertheless.h
+//  Nevertheless
+//
+//  Created by Ben Gottlieb on 6/10/17.
+//  Copyright © 2017 Ben Gottlieb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nevertheless.
+FOUNDATION_EXPORT double NeverthelessVersionNumber;
+
+//! Project version string for Nevertheless.
+FOUNDATION_EXPORT const unsigned char NeverthelessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nevertheless/PublicHeader.h>
+
+
